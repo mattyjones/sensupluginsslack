8714a89f6ac050b5544d7ed9f8f7d4c14ff817bc
Matt Jones <mattjones@yieldbot.com>
2015-11-25 12:46:50 -0500

    massive cleanup and refactor to bring in line w/ standards

--- src/handler-yieldbot-slack.go
+++ src/handler-yieldbot-slack.go
@@ -8,19 +8,17 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"fmt"
 	"github.com/nlopes/slack"
-	"github/yieldbot/dhuran"
-	"github/yieldbot/dracky"
-	"io/ioutil"
+	dracky "github.com/yieldbot/sensu-yieldbot-library/src"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 )
 
-func set_color(status int) string {
+func setColor(status int) string {
 	switch status {
 	case 0:
 		return "#33CC33"
@@ -35,90 +33,78 @@ func set_color(status int) string {
 	}
 }
 
-func clean_output(output string) string {
+func cleanOutput(output string) string {
 	return strings.Split(output, ":")[0]
+}
+
+func acquireChannelID(channel string) string {
+	fmt.Printf("%v", channel)
+	return channel
 }
 
 func main() {
 
-	slack_tokenPtr := flag.String("token", "", "the slack integration token")
+	slackTokenPtr := flag.String("token", "", "the slack integration token")
 	channelPtr := flag.String("channel", "monitoring-test", "the channel to post notifications to")
-	stdinPtr := flag.Bool("read-stdin", true, "read input from stdin")
-	input_filePtr := flag.String("input-file", "", "file to read json in from, check docs for proper format")
+	// stdinPtr := flag.Bool("read-stdin", true, "read input from stdin")
 
 	flag.Parse()
 
-	slack_token := *slack_tokenPtr
+	slackToken := *slackTokenPtr
 	channel := *channelPtr
-	rd_stdin := *stdinPtr
-	input_file := *input_filePtr
+	// rd_stdin := *stdinPtr
 
-	// I don't want to call these if they are not needed
-	sensu_event := new(dracky.Sensu_Event)
-	user_event := new(dracky.User_Event)
+	sensuEvent := new(dracky.SensuEvent)
+	sensuEvent = sensuEvent.AcquireSensuEvent()
+
+	_ = acquireChannelID(channel)
 
 	// YELLOW
 	// this is ugly, needs to be a better way to do this
-	if slack_token == "" {
+	if slackToken == "" {
 		fmt.Print("Please enter a slack integration token")
 		os.Exit(1)
 	}
 
-	if (rd_stdin == false) && (input_file != "") {
-		user_input, err := ioutil.ReadFile(input_file)
-		if err != nil {
-			dhuran.Check(err)
-		}
-		err = json.Unmarshal(user_input, &user_event)
-		if err != nil {
-			dhuran.Check(err)
-		}
-	} else if (rd_stdin == false) && (input_file == "") {
-		fmt.Printf("Please enter a file to read from")
-		os.Exit(1)
-	} else {
-		sensu_event = sensu_event.Acquire_sensu_event()
-	}
-
-	api := slack.New(slack_token)
+	api := slack.New(slackToken)
 	params := slack.PostMessageParameters{}
 	attachment := slack.Attachment{
-		Color: set_color(sensu_event.Check.Status),
+		Color: setColor(sensuEvent.Check.Status),
 
 		Fields: []slack.AttachmentField{
 			slack.AttachmentField{
 				Title: "Monitored Instance",
-				Value: sensu_event.Acquire_monitored_instance(),
+				Value: sensuEvent.AcquireMonitoredInstance(),
 				Short: true,
 			},
 			slack.AttachmentField{
 				Title: "Sensu Client",
-				Value: sensu_event.Client.Name,
+				Value: sensuEvent.Client.Name,
 				Short: true,
 			},
 			slack.AttachmentField{
 				Title: "Check Name",
-				Value: dracky.Create_check_name(sensu_event.Check.Name),
+				Value: dracky.CreateCheckName(sensuEvent.Check.Name),
 				Short: true,
 			},
 			slack.AttachmentField{
 				Title: "Check State",
-				Value: dracky.Define_status(sensu_event.Check.Status),
+				Value: dracky.DefineStatus(sensuEvent.Check.Status),
 				Short: true,
 			},
 			slack.AttachmentField{
 				Title: "Event Time",
-				Value: time.Unix(sensu_event.Check.Issued, 0).Format(time.RFC822Z),
+				Value: time.Unix(sensuEvent.Check.Issued, 0).Format(time.RFC3339),
 				Short: true,
 			},
 			slack.AttachmentField{
 				Title: "Check State Duration",
-				Value: dracky.Define_check_state_duration(),
+				Value: strconv.Itoa(dracky.DefineCheckStateDuration()),
 				Short: true,
 			},
 			slack.AttachmentField{
 				Title: "Check Output",
-				Value: clean_output(sensu_event.Check.Output),
+				Value: cleanOutput(sensuEvent.Check.Output),
 				Short: true,
 			},
 		},

1d8550e7c6b20e2035c226d4946258a37918e940
Matt Jones <mattjones@yieldbot.com>
2016-02-17 16:32:17 -0500

    convert to cli and add proper flags

--- cmd/handler-slack/handler.go
+++ cmd/handler-slack/handler.go
@@ -8,30 +8,16 @@
 package main
 
 import (
-	"flag"
 	"fmt"
+	"github.com/codegangsta/cli"
 	"github.com/nlopes/slack"
 	"github.com/yieldbot/sensuplugin/sensuhandler"
 	"github.com/yieldbot/sensuplugin/sensuutil"
+	"os"
 	"strconv"
 	"strings"
 	"time"
 )
-
-func setColor(status int) string {
-	switch status {
-	case 0:
-		return "#33CC33"
-	case 1:
-		return "warning"
-	case 2:
-		return "#FF0000"
-	case 3:
-		return "#FF6600"
-	default:
-		return "#FF6600"
-	}
-}
 
 func cleanOutput(output string) string {
 	return strings.Split(output, ":")[0]
@@ -39,73 +25,90 @@ func cleanOutput(output string) string {
 
 func main() {
 
-	slackTokenPtr := flag.String("token", "", "the slack integration token")
-	// channelPtr := flag.String("channel", "monitoring-test", "the channel to post notifications to")
+	var slackToken string
+	var channelID string
 
-	flag.Parse()
+	app := cli.NewApp()
+	app.Name = "handler-slack"
+	app.Usage = "Send notifications to a given Slack channel as an attachment"
+	app.Action = func(c *cli.Context) {
 
-	slackToken := *slackTokenPtr
-	// channelName := *channelPtr
-	channelID := "C09JY7W0P"
+		sensuEvent := new(sensuhandler.SensuEvent)
+		sensuEvent = sensuEvent.AcquireSensuEvent()
 
-	sensuEvent := new(sensuhandler.SensuEvent)
-	sensuEvent = sensuEvent.AcquireSensuEvent()
+		if slackToken == "" {
+			fmt.Print("Please enter a slack integration token")
+			sensuutil.Exit("CONFIGERROR")
+		}
 
-	if slackToken == "" {
-		fmt.Print("Please enter a slack integration token")
-		sensuutil.Exit("CONFIGERROR")
+		fmt.Printf(slackToken)
+
+		api := slack.New(slackToken)
+		params := slack.PostMessageParameters{}
+		attachment := slack.Attachment{
+			Color: sensuhandler.SetColor(sensuEvent.Check.Status),
+
+			Fields: []slack.AttachmentField{
+				slack.AttachmentField{
+					Title: "Monitored Instance",
+					Value: sensuEvent.AcquireMonitoredInstance(),
+					Short: true,
+				},
+				slack.AttachmentField{
+					Title: "Sensu Client",
+					Value: sensuEvent.Client.Name,
+					Short: true,
+				},
+				slack.AttachmentField{
+					Title: "Check Name",
+					Value: sensuhandler.CreateCheckName(sensuEvent.Check.Name),
+					Short: true,
+				},
+				slack.AttachmentField{
+					Title: "Check State",
+					Value: sensuhandler.DefineStatus(sensuEvent.Check.Status),
+					Short: true,
+				},
+				slack.AttachmentField{
+					Title: "Event Time",
+					Value: time.Unix(sensuEvent.Check.Issued, 0).Format(time.RFC3339),
+					Short: true,
+				},
+				slack.AttachmentField{
+					Title: "Check State Duration",
+					Value: strconv.Itoa(sensuhandler.DefineCheckStateDuration()),
+					Short: true,
+				},
+				slack.AttachmentField{
+					Title: "Check Output",
+					Value: cleanOutput(sensuEvent.Check.Output),
+					Short: true,
+				},
+			},
+		}
+		params.Attachments = []slack.Attachment{attachment}
+		channelID, timestamp, err := api.PostMessage(channelID, "", params)
+		if err != nil {
+			sensuutil.EHndlr(err)
+		}
+		fmt.Printf("Message successfully sent to channel %s at %s", channelID, timestamp)
 	}
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:        "token, t",
+			Value:       "",
+			Usage:       "he slack integration toke",
+			EnvVar:      "SLACK_TOKEN",
+			Destination: &slackToken,
+		},
 
-	fmt.Printf(slackToken)
-	// fmt.Printf(channelName)
-
-	api := slack.New(slackToken)
-	params := slack.PostMessageParameters{}
-	attachment := slack.Attachment{
-		Color: setColor(sensuEvent.Check.Status),
-
-		Fields: []slack.AttachmentField{
-			slack.AttachmentField{
-				Title: "Monitored Instance",
-				Value: sensuEvent.AcquireMonitoredInstance(),
-				Short: true,
-			},
-			slack.AttachmentField{
-				Title: "Sensu Client",
-				Value: sensuEvent.Client.Name,
-				Short: true,
-			},
-			slack.AttachmentField{
-				Title: "Check Name",
-				Value: sensuhandler.CreateCheckName(sensuEvent.Check.Name),
-				Short: true,
-			},
-			slack.AttachmentField{
-				Title: "Check State",
-				Value: sensuhandler.DefineStatus(sensuEvent.Check.Status),
-				Short: true,
-			},
-			slack.AttachmentField{
-				Title: "Event Time",
-				Value: time.Unix(sensuEvent.Check.Issued, 0).Format(time.RFC3339),
-				Short: true,
-			},
-			slack.AttachmentField{
-				Title: "Check State Duration",
-				Value: strconv.Itoa(sensuhandler.DefineCheckStateDuration()),
-				Short: true,
-			},
-			slack.AttachmentField{
-				Title: "Check Output",
-				Value: cleanOutput(sensuEvent.Check.Output),
-				Short: true,
-			},
+		cli.StringFlag{
+			Name:        "channel, c",
+			Value:       "",
+			Usage:       "The channel ID that you wish to send this to",
+			EnvVar:      "SLACK_CHANNEL_ID",
+			Destination: &channelID,
 		},
 	}
-	params.Attachments = []slack.Attachment{attachment}
-	channelID, timestamp, err := api.PostMessage(channelID, "", params)
-	if err != nil {
-		sensuutil.EHndlr(err)
-	}
-	fmt.Printf("Message successfully sent to channel %s at %s", channelID, timestamp)
+	app.Run(os.Args)
 }
